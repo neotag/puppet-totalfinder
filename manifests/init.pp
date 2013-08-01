cbdcc3fb5265863c26839170d5a6f55e230e2925
Naotaka Harada <harada@gaji.jp>
2013-08-01 15:20:31 +0900

    Add ensure.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,6 @@ class totalfinder {
   package { 'TotalFinder':
     ensure   => installed,
     source   => 'http://downloads.binaryage.com/TotalFinder-1.4.18.dmg',
-    provider => appdmg,
+    provider => pkgdmg,
   }
 }
