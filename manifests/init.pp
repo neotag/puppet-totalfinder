aedb667a32d94301a50c99c085e51b905ee7b143
Naotaka Harada <harada@gaji.jp>
2013-08-01 15:13:37 +0900

    Fixed provider.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,7 @@
 # This is a placeholder class.
 class totalfinder {
   package { 'TotalFinder':
-    ensure   => installed,
     source   => 'http://downloads.binaryage.com/TotalFinder-1.4.18.dmg',
-    provider => 'pkgdmg'
+    provider => 'appdmg',
   }
 }
